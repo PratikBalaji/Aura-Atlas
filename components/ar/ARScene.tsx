e8dd8798d080fa01a0fe303a5447bdbbbf6ec843
Pratik Balaji <60303726+PratikBalaji@users.noreply.github.com>
2026-03-21 15:21:38 -0400

    Import XR, Controllers, and Hands from @react-three/xr

--- components/ar/ARScene.tsx
+++ components/ar/ARScene.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { XRButton } from '@react-three/xr';
+import { XRButton, XR, Controllers, Hands } from '@react-three/xr';
 import { PerspectiveCamera, OrbitControls, Environment, Sky, Stars } from "@react-three/drei";
 import { useState, useEffect, useMemo, useRef } from "react";
 import * as THREE from "three";
